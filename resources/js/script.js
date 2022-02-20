1147b7a17d6c52272b644306c8368f8479b79bb9
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-02-20 23:22:44 +0000

    Animating the text opposite the Portfolio Picture

---
+++ resources/js/script.js
@@ -0,0 +1,42 @@
+
+// let text = ["Yahaya", "Abubakar", "Sadick"];
+// let counter = 0;
+// let elem = document.getElementById("text-logo");
+// let inst = setInterval(change, 1000);
+
+// function change() {
+//   elem.innerHTML = text[counter];
+//   counter++;
+//   if (counter >= text.length) {
+//     counter = 0;
+//     clearInterval(inst); 
+//   }
+// }
+
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
